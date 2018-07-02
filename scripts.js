aa07569dc347ac3ea8387e1322f9195f4febc390
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-07-02 16:40:25 +0200

    add on-click behavior, update styles

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// var titleElem = document.getElementsByTagName('h1');
+// console.log(titleElem);
+// titleElem.addEventListener('click', function() {
+//     showMessage();
+// });
+
+function showMessage() {
+    alert('Hello World');
+}
