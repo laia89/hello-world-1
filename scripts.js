d71c333ce073db44d19d9d4ddce09c34f3a508bb
laia89 <laiacy234@gmail.com>
2018-07-03 12:07:07 +0200

    add welcome alert

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 
 var titleElem = document.getElementsByTagName('h1')[0];
+
 titleElem.addEventListener('click', function() {
     showMessage();
 });
 
 function showMessage() {
     alert('Hello, World! Hola, Mundo! Hallo, Welt!');
+}
+
+function showWelcomeMsg() {
+    alert('Welcome, Bienvenido, Willkommen');
 }
